08f0ba6b32d640a9fa8673eded3dc2d8256558d3
Caroline Cerussi <caroceru@gmail.com>
2022-09-09 17:10:57 -0700

    WIP submit edited component code to get to show on localhost .. WIP= need to update readme and make sure the page is able to be navigated

--- src/components/EditCoffeeBeanForm.js
+++ src/components/EditCoffeeBeanForm.js
@@ -1,55 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import CoffeeBean from "./CoffeeBean";
+import ReusableForm from "./ReusableForm";
 
-const EditCoffeeBeanForm = (props) => {
+function EditCoffeeBeanForm (props) { 
 	const {coffeeBean} = props;
-  return (
-    <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input required='true'
-          type='text'
-          name='name'
-          placeholder='Name' />
-        <input required='true'
-          type='text'
-          name='origin'
-          placeholder='Origin' />
-					<input required='true'
-          name='roast' placeholder="Select Roast">
-          <select requred='true' id="dropdown">
-            <option value="Light">Light</option>
-            <option value="Medium">Medium</option>
-            <option value="Dark">Dark</option>
-          </select>
-          </input> 
-				  <input required='true'
-          type='text'
-          name='price'
-          placeholder='Price (per bag)' />
-        <textarea required='true'
-          name='description'
-          placeholder='Flavor Description' />
-        <button type='submit'>{props.buttonText}</button>
-      </form>
-    </React.Fragment>
-  )
-}
-
-EditCoffeeBeanForm.propTypes = {
-coffeeBean: PropTypes.object,
-onEditCoffeeBean: PropTypes.func
-};
-
-export default EditCoffeeBeanForm;
-
-
-
-
-
-function EditCoffeeBeanForm(props) {
-	const {coffeeBean} = props;
-
+	
 	function handleEditCoffeeBeanFormSubmission(event) {
 		event.preventDefault();
 		props.onEditCoffeeBean({
@@ -58,10 +13,21 @@ function EditCoffeeBeanForm(props) {
 			roast: event.target.origin.value,
 			price: event.target.price.value,
 			description : event.target.description.value,
-			id: coffeeBean.id});
+			id: coffeeBean.id
+		});
 		}
 		return (
 			<React.Fragment>
 			<ReusableForm 
 			formSubmissionHandler= {handleEditCoffeeBeanFormSubmission}
 				buttonText="Update Coffee Details" />
+				</React.Fragment>
+		);
+}
+
+EditCoffeeBeanForm.propTypes ={
+	coffeeBean: PropTypes.object,
+	onEditCoffeeBean: PropTypes.func
+};
+
+export default EditCoffeeBeanForm;
